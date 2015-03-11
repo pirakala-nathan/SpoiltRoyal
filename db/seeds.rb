17b082afba2f690247e74d7a8a1423ede96b8b4b
Spondon <asbanday@uwaterloo.ca>
2015-03-11 17:25:16 -0400

    merge errrors

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,25 +27,6 @@ admin = User.create(
   admin: false
 )
 
-<<<<<<< HEAD
-
-# test1 = User.create(
-#   username: "test1",
-#   email: "spon121@gmail.com",
-#   password: "test",
-#   password_confirmation: "test",
-#   account_type: "Vendor",
-#   admin: false
-# )
-# test2 = User.create(
-#   username: "test2",
-#   email: "spon121@gmail.com",
-#   password: "test",
-#   password_confirmation: "test",
-#   account_type: "Vendor",
-#   admin: false
-# )
-=======
 test1 = User.create(
   username: "test1",
   email: "user1@test.com",
@@ -62,7 +43,7 @@ test2 = User.create(
   account_type: "Vendor",
   admin: false
 )
->>>>>>> 8aee63839370ffb94bf24cc31bc377b01bd1a5cc
+
 
 TimedTask.create(interval: 1, measure_of_time: "minute")
 
